91af4673d7548c7991687a0118370ab7812918c3
t0b1 <39978666+Kuber12@users.noreply.github.com>
2019-06-15 22:22:40 +0545

    Add files via upload

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ async def highlow(ctx):
 async def bank(ctx):
     cards=["ace","two","three","four","five","six","seven","eight","nine","ten","jack","queen","king"]
     suits=["of hearts","of clubs","of diamonds","of spades"]
-    await ctx.send(f'{random.cards} {random.suits}')
+    await ctx.send(f'{random.choice(cards)} {random.choice(suits)}')
        
     
 client.run('NTg3NzA3ODYwMDE2NjkzMjU4.XQJ7Bg.BgoZHKatBm5ctjAplPt45tFTSX0')
