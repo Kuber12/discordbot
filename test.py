36ad5c93eedfd1a33d2a8c5b1315da96e3fae331
t0b1 <39978666+Kuber12@users.noreply.github.com>
2019-06-16 13:34:22 +0545

    Add files via upload

--- test.py
+++ test.py
@@ -78,7 +78,7 @@ async def highlow(ctx):
 
 @client.command()
 async def bank(ctx):
-    cards=['ace':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10,'jack':11,'queen':12,'king':13]
+    cards=['ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king']
     suits=[' of hearts',' of clubs',' of diamonds',' of spades']
     firstCard = random.choice(cards) + random.choice(suits)
     secondCard = random.choice(cards) + random.choice(suits)
